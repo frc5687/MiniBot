9611f46e3ca78516261d41fd5392c879b445fd2f
jackellingwood <jack.ellingwood@baxter-academy.org>
2022-09-28 16:32:54 -0400

    updated can might work

--- src/main/java/org/frc5687/swerve/RobotMap.java
+++ src/main/java/org/frc5687/swerve/RobotMap.java
@@ -11,13 +11,13 @@ public class RobotMap {
      */
     public static class CAN {
 
-        public static class TALONFX {
-            public static final int BL_RIGHT_FALCON = 3; // real 3
-            public static final int BL_LEFT_FALCON = 4; // real 4
-            public static final int BR_RIGHT_FALCON = 1;
-            public static final int BR_LEFT_FALCON = 2;
-            public static final int FL_RIGHT_FALCON = 5; // real 5
-            public static final int FL_LEFT_FALCON = 6; // real 6
+        public static class TALONFX { //updated for minibot
+            public static final int BL_RIGHT_FALCON = 4;
+            public static final int BL_LEFT_FALCON = 3;
+            public static final int BR_RIGHT_FALCON = 6;
+            public static final int BR_LEFT_FALCON = 5;
+            public static final int FL_RIGHT_FALCON = 1;
+            public static final int FL_LEFT_FALCON = 2;
             public static final int FR_RIGHT_FALCON = 7;
             public static final int FR_LEFT_FALCON = 8;
         }
@@ -52,9 +52,9 @@ public class RobotMap {
      * for DIO only one device can connect to each port, so the numbers should be unique.
      */
     public static class DIO {
-        public static final int ENCODER_FR = 4;
-        public static final int ENCODER_FL = 3;
-        public static final int ENCODER_BR = 5;
-        public static final int ENCODER_BL = 2;
+        public static final int ENCODER_FR = 3;
+        public static final int ENCODER_FL = 0;
+        public static final int ENCODER_BR = 2;
+        public static final int ENCODER_BL = 1;
     }
 }
